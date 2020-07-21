a6028a84518e0fd8e5becb153294bdb3a4e457b1
Karol P <karprzy7@gmail.com>
2020-07-21 21:44:44 +0200

    Better logging and retries for BLE connection

--- bt_python/bt_getter.py
+++ bt_python/bt_getter.py
@@ -1,26 +1,37 @@
 from bluepy import btle
 from time import sleep
-import struct
+import logging
+import sys
+logging.basicConfig(level = logging.DEBUG)
+
+logger = logging.getLogger(__name__)
 
 
 if __name__ == "__main__":
-    print ("Connecting...")
-    dev = btle.Peripheral("DB:1A:7C:4D:6D:4B", addrType="random")
+    
 
-    raw = dev.readCharacteristic(0x17)
-    print(raw.hex())
-    raw = dev.readCharacteristic(0x17)
-    print(raw)
+    for attempt in range(10):
+        try:
+            logger.debug("Connecting...")
+            dev = btle.Peripheral("DB:1A:7C:4D:6D:4B", addrType="random")
+        except Exception as e:
+            logger.error("Connection failed due to: {}, reconnecting...".format(e))
+            sleep(2)
+        else:
+            break
+    else:
+        logger.error("Connection failed after all attempts. Examine remote sensor.")
+        sys.exit()
+    
 
-    print ("Services...")
+    logger.debug("Services...")
     for svc in dev.services:
-        print (str(svc))
+        logger.debug(str(svc))
         svc.getCharacteristics()
 
 
     tempService = dev.getServiceByUUID("0000181a-0000-1000-8000-00805f9b34fb")
-    temp_char = tempService.getCharacteristics()
+    temp_char = tempService.getCharacteristics()[0]
 
-    for char in temp_char:
-        print("Characteristic handle: {}".format(hex(char.valHandle)))
-        print("Characteristic value: {}".format(char.read()))
+    raw_temp = temp_char.read()
+    logger.debug("Raw temperature: {}".format(raw_temp.hex()))
