2cb51682543fbe8a18cd32432eeba440193e5b60
Karol P <karprzy7@gmail.com>
2020-07-22 22:36:28 +0200

    Added ble_logger service

---
+++ rpi_ble_logger_service/install.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+cp ../bt_python/bt_getter.py /usr/bin/bt_getter.py
+chmod +x /usr/bin/bt_getter.py
+
+cp ble_logger.service /etc/systemd/system/ble_logger.service
+chmod 644 /etc/systemd/system/ble_logger.service
