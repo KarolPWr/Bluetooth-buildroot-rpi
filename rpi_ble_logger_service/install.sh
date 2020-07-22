1d5bafd20bcff99b086f4732606711cce85ed059
Karol P <karprzy7@gmail.com>
2020-07-22 23:11:21 +0200

    Added ble_logger.timer - working

--- rpi_ble_logger_service/install.sh
+++ rpi_ble_logger_service/install.sh
@@ -5,3 +5,6 @@ chmod +x /usr/bin/bt_getter.py
 
 cp ble_logger.service /etc/systemd/system/ble_logger.service
 chmod 644 /etc/systemd/system/ble_logger.service
+
+cp ble_logger.timer /etc/systemd/system/ble_logger.timer
+chmod 644 /etc/systemd/system/ble_logger.timer
